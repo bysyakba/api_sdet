cf6b9f7a90e7205732bae229f51264652b41b7f7
Egor <tyfg6594@gmail.com>
2025-02-03 22:35:46 +0400

    1.0

---
+++ tests/baskend/create_project.py
@@ -0,0 +1,35 @@
+import requests
+import pytest
+
+
+BASE_URL = 'http://admin:admin@localhost:8111'
+
+
+class TestProjectCreate:
+
+    def test_project_create(self):
+        # Получение токена
+        auth_response = requests.get(url=f"{BASE_URL}/authenticationTest.html?csrf", auth=("admin", "admin"))
+        csrf_token = auth_response.text
+        headers = {"X-TC-CSRF-Token": csrf_token}
+
+        # Подготовка данных
+        project_id="simpleprojectID3"
+        project_data= {
+                        "parentProject": {
+                                            "locator": "_Root"
+                                         },
+                        "name": "ProjectNameSimple3",
+                        "id": project_id,
+                        "copyAllAssociatedSettings": True
+                      }
+        # Создание проекта
+        create_responce = requests.post(url=f"{BASE_URL}/app/rest/projects", headers=headers, json=project_data)
+        assert create_responce.status_code == 200, "Не удалось создать проект"
+
+        check_project = requests.get(url=f"{BASE_URL}/app/rest/projects/id:{project_id}",headers=headers)
+        assert check_project.status_code == 200
+
+        # Удаление проекта
+        delete_project = requests.delete(url=f"{BASE_URL}/app/rest/projects/id:{project_id}", headers=headers)
+        assert delete_project.status_code == 204, "Не удалось удалить проект"
