70b2b95c37846ec284ae627050d3d6bfc2fe483c
Egor <tyfg6594@gmail.com>
2025-02-24 22:17:34 +0400

    1336

--- pages/setup_page.py
+++ pages/setup_page.py
@@ -1,6 +1,7 @@
 import allure
 # from pages.base_page import BasePage
 from actions.page_actions import PageActions
+from constants.hosts import BASE_URL
 
 
 class FirstStartWindow(PageActions):
@@ -22,12 +23,10 @@ class Loading(PageActions):
         self.loading_icon_locator = ".icon-refresh"
 
     def wait_loading(self
-                     # , timeout_wait=1000
-                     , timeout_disappear=1000000):
+                     , timeout_wait=1000000000
+                     , timeout_disappear=1000000000):
         with allure.step("Ждем начала лоадинга"):
-            self.wait_for_selector(self.loading_icon_locator
-                                   # , timeout_wait
-                                   )
+            self.wait_for_selector(self.loading_icon_locator, timeout_wait)
         with allure.step("Ждем когда закончится лоадинг"):
             self.wait_disappear_selector(self.loading_icon_locator, timeout_disappear)
 
@@ -36,9 +35,10 @@ class Agreement(PageActions):
     def __init__(self, page):
         super().__init__(page)
         self.page = page
-        self.page_url = f"/showAgreement.html"
-        self.accept_checkbox_locator = "input#accept"
-        self.continue_button_locator = ".submitButton"
+        self.page_url = "/showAgreement.html"
+        self.page_setup = f"{BASE_URL}/setupAdmin.html"
+        self.accept_checkbox_locator = "#accept"
+        self.continue_button_locator = ".btn"
 
     def check_in_box(self):
         with allure.step("Активируем чекбокс"):
@@ -56,7 +56,7 @@ class SetUpUser(PageActions):
         self.username_locator = "#input_teamcityUsername"
         self.password_locator = "#password1"
         self.confirm_password_locator = "#retypedPassword"
-        self.create_button = ".loginButton"
+        self.create_button = ".btn"
 
     def fill_user_data(self, user_name, user_password):
         self.input_text(self.username_locator, user_name)
@@ -75,6 +75,23 @@ class SetUpPage(PageActions):
         self.agreement = Agreement(self.page)
         self.setup_user = SetUpUser(self.page)
 
+    # def set_up2(self, username="admin", password="lox"):
+    #     self.navigate(self.page_url)
+    #     self.wait_for_page_load()
+    #     self.first_start_window.proceed_step()
+    #     self.loading.wait_loading()
+    #     self.first_start_window.proceed_step()
+    #     self.loading.wait_loading()
+    #     self.check_url(self.agreement.page_url)
+    #     self.agreement.check_in_box()
+    #     self.agreement.continue_agreement()
+    #     self.wait_for_url_change(self.agreement.page_url)
+    #     self.wait_for_page_load()
+    #     self.setup_user.fill_user_data(username, password)
+    #     self.setup_user.create_user()
+    #     self.wait_for_url_change(self.setup_user.page_url)
+
+
     def set_up(self, username="admin", password="lox"):
         self.navigate(self.page_url)
         self.wait_for_page_load()
@@ -85,8 +102,8 @@ class SetUpPage(PageActions):
         self.check_url(self.agreement.page_url)
         self.agreement.check_in_box()
         self.agreement.continue_agreement()
-        self.wait_for_url_change(self.agreement.page_url)
+        self.wait_for_url_change(self.agreement.page_setup)
         self.wait_for_page_load()
         self.setup_user.fill_user_data(username, password)
         self.setup_user.create_user()
-        self.wait_for_url_change(self.setup_user.page_url)
+        self.wait_for_url_change(f"{BASE_URL}/favorite/projects")
